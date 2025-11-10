dee7063ecc0afc08267541ad383a28cec1a4b6b3
Arsen <archi1504@gmail.com>
2025-11-10 19:23:52 +0300

    fitered done

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -6,13 +6,31 @@ console.log('Base URL:', import.meta.env.VITE_NEWS_BASE_API_URL);
 const BASE_URL = 'https://api.currentsapi.services/v1';
 const API_KEY = 'S6ns1XyKpARWfqvG51wCQtB20KfD2S9w4nG-TO3GWbZYvia3';
 
-export const getNews = async (page_number = 1, page_size = 10) => {
+export const getNews = async ({
+  page_number = 1,
+  page_size = 10,
+  category,
+}) => {
   try {
     const response = await axios.get(`${BASE_URL}/search`, {
       params: {
         apiKey: API_KEY,
         page_number,
         page_size,
+        category,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/availabel/categories`, {
+      params: {
+        apiKey: API_KEY,
       },
     });
     return response.data;
