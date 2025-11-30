4ee0ae014046af9ce545b78e6092f2f15aa7f893
Arsen <archi1504@gmail.com>
2025-11-30 16:24:17 +0300

    feat: add theme icons with proper types

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,4 +1,5 @@
 import { getCategories } from '../../api/apiNews';
+import { useTheme } from '../../context/ThemeContext';
 import { useFetch } from '../../helpers/hooks/useFetch';
 import { CategoriesApiResponce, IFilters } from '../../Interfaces';
 import Categories from '../Categories/Categories';
@@ -12,13 +13,14 @@ interface Props {
 }
 
 const NewsFilters = ({ filters, changeFilter }: Props) => {
+  const { isDark } = useTheme();
   const { data: dataCategories } = useFetch<CategoriesApiResponce, null>(
     getCategories
   );
   return (
     <div className={styles.filters}>
       {dataCategories ? (
-        <Slider>
+        <Slider isDark={isDark}>
           <Categories
             categories={dataCategories.categories}
             selectedCategory={filters.category}
